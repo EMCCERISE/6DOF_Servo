03e0c4f7dcb979c93d0e1223e4d6afd532d7220d
alsnac <alsnac@gmail.com>
2024-10-10 10:19:22 -0300

    melhora na estrutura

--- ROS2-urdf/build/servo6dof/CMakeFiles/servo6dof_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/servo6dof_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/servo6dof_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
